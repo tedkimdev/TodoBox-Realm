59e55091f8f9e6a8a3646489370385a9a9ea558c
aney <yenafirst91@gmail.com>
2017-03-25 00:54:34 +0900

    Add UI

--- TodoBox-Realm/TaskEditViewController.swift
+++ TodoBox-Realm/TaskEditViewController.swift
@@ -10,12 +10,61 @@ import UIKit
 
 final class TaskEditViewController: UIViewController {
   
+  // MARK: Constants
+  
+  struct Font {
+    static let textField = UIFont.systemFont(ofSize: 13)
+  }
+  
+  // MARK: UI
+  
+  fileprivate let doneButtonItem = UIBarButtonItem(
+    barButtonSystemItem: .done,
+    target: nil,
+    action: nil
+  )
+  fileprivate let textField = UITextField(frame: .zero)
+  
+  // MARK: Initializing
+  
   init() {
     super.init(nibName: nil, bundle: nil)
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  // MARK: View Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.title = "Add To Do Item"
+    
+    self.navigationItem.rightBarButtonItem = self.doneButtonItem
+    self.doneButtonItem.target = self
+    self.doneButtonItem.action = #selector(doneButtomItemDidTap)
+    
+    self.textField.borderStyle = .roundedRect
+    self.textField.placeholder = "Input to do what"
+    
+    self.view.addSubview(self.textField)
+    
+    self.textField.snp.makeConstraints { make in
+      make.top.equalToSuperview().offset(100)
+      make.left.equalToSuperview().offset(10)
+      make.right.equalToSuperview().offset(-10)
+      make.height.equalTo(Font.textField.lineHeight * 2)
+    }
+  }
+  
+  
+  // MARK: Actions
+  
+  func doneButtomItemDidTap() {
+    print("doneButtomItemDidTap")
   }
   
 }
